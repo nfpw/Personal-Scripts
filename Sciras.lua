1bbb33b67a8b4be489c163a230878d4fbdec81fa
nfpw <153939922+nfpw@users.noreply.github.com>
2024-01-01 14:13:23 +0300

    Update Sciras.lua

--- Sciras.lua
+++ Sciras.lua
@@ -1,23 +1,18 @@
 while wait() do
-	if game.Loaded then
 		local lp = game.Players.LocalPlayer
 		local lpCh = lp.Character
 		local Human = lpCh.HumanoidRootPart
 		local humancf = Human.CFrame
 		local NoTES = game.Workspace.Notes
-	end
 end
 
 local esp = Instance.new("Highlight")
 
-if game.Loaded then
 	_G.Farm = true
 	_G.WaitTime = 0.3
 	_G.GetTime = 0.3
 	_G.Method = "Bring" -- eğer Tp istiyosan değiştir
-end
 
-if game.Loaded then
 	for i, v in pairs(NoTES:GetDescendants()) do
 		_G.GidilicekYer = v.CFrame
         if v.Name:FindFirstChild("esp") then
@@ -31,9 +26,8 @@ if game.Loaded then
         end
 		wait(_G.GetTime)
 	end
-end
 
-while _G.Farm == true and game.Loaded do
+while _G.Farm == true do
 	if _G.Method == "Tp" then
 	humancf = _G.GidilicekYer
 	wait(_G.WaitTime)
