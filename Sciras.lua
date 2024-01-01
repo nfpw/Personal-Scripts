416d7e9eb4f3b765d99ed2f761204ddf372c758c
nfpw <153939922+nfpw@users.noreply.github.com>
2024-01-01 14:25:19 +0300

    Update Sciras.lua

--- Sciras.lua
+++ Sciras.lua
@@ -1,38 +1,28 @@
 while wait() do
-	local lp = game.Players.LocalPlayer
-	local lpCh = lp.Character
-	local Human = lpCh.HumanoidRootPart
-	local humancf = Human.CFrame
-	local NoTES = game.Workspace.Notes
-end
+    local lp = game.Players.LocalPlayer
+    local lpCh = lp.Character
+    local Human = lpCh.HumanoidRootPart
+    local NoTES = game.Workspace.Notes
 
-local esp = Instance.new("Highlight")
+    _G.Farm = true
+    _G.WaitTime = 0.3
+    _G.GetTime = 0.3
+    _G.Method = "Bring" -- Change to "Tp" if you want teleportation
 
-_G.Farm = true
-_G.WaitTime = 0.3
-_G.GetTime = 0.3
-_G.Method = "Bring" -- eğer Tp istiyosan değiştir
+    for i, v in pairs(NoTES:GetDescendants()) do
+        if v:IsA("BasePart") then
 
-for i, v in pairs(NoTES:GetDescendants()) do
-	_G.GidilicekYer = v.CFrame
-	if v.Name:FindFirstChild("esp") then
-		print("it has esp")
-		wait(.1)
-	else
-		esp.FillTransparency = 1
-		esp:Clone().Parent = v.Name
-		esp.OutlineColor = Color3.fromRGB(255, 255, 255)
-		wait(.1)
-	end
-	wait(_G.GetTime)
-	if _G.Farm == true do
-			if _G.Method == "Tp" then
-				humancf = _G.GidilicekYer
-				wait(_G.WaitTime)
-			else
-				v.CFrame = humancf
-				wait(_G.WaitTime)
-			end
-		end
-	end
+            wait(_G.GetTime)
+
+            if _G.Farm then
+                if _G.Method == "Tp" then
+                    Human.CFrame = v.CFrame
+                    wait(_G.WaitTime)
+                else
+                    Human.CFrame = v.CFrame
+                    wait(_G.WaitTime)
+                end
+            end
+        end
+    end
 end
